854c8d9e085d9b0d3f9d92f8626d6da33ce85392
koki1019 <lukaodzelashvili@gmail.com>
2023-05-01 11:42:00 +0100

    Test

--- Vault Engine.sh
+++ Vault Engine.sh
@@ -5,4 +5,5 @@ export LD_LIBRARY_PATH="$cwd/lib"
 clear
 # valgrind --leak-check=yes --log-file=valgrind.rpt ./build.out
 # MANGOHUD_CONFIG="cpu_temp,gpu_temp,core_load,engine_version,ram,vram" mangohud --dlsym ./bin/build.out /home/koki1019/Desktop/Projects/Polyjam_Game
-./bin/build.out
+./bin/build.out /home/koki1019/Desktop/Projects/Polyjam_Game
+# ./bin/build.out
