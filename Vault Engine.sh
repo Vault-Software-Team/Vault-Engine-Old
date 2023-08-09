71f67198ba7a1e8ef65a61e58230fb97ef512585
koki1019 <lukaodzelashvili@gmail.com>
2023-08-09 22:03:52 +0100

    fix vault engine.sh

--- Vault Engine.sh
+++ Vault Engine.sh
@@ -4,7 +4,7 @@ cwd=$(pwd)
 export LD_LIBRARY_PATH="$cwd/lib"
 clear
 # valgrind --leak-check=yes --log-file=valgrind.rpt ./build.out
-MANGOHUD_CONFIG="cpu_temp,gpu_temp,core_load,engine_version,ram,vram" mangohud --dlsym ./bin/game.out
+# MANGOHUD_CONFIG="cpu_temp,gpu_temp,core_load,engine_version,ram,vram" mangohud --dlsym ./bin/game.out
 # ./bin/build.out /home/koki1019/Desktop/Projects/Polyjam_Game
 # gdb --args ./bin/build.out "/home/koki1019/.vault_projects/NPC"
 ./bin/build.out
